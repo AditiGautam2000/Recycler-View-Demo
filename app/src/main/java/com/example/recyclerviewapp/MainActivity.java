1ea762de66f674485f396f9004a2de47517ecd88
Aditi Gautam <aditi.ddn46@gmail.com>
2021-08-07 18:52:19 +0530

    Recycler View Demo

---
+++ app/src/main/java/com/example/recyclerviewapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.recyclerviewapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+RecyclerView recyclerView;
+    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView=findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        CustomAdapter c=new CustomAdapter(cars);
+        recyclerView.setAdapter(c);
+
+    }
+}
